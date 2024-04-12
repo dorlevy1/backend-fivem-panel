60cd5228f4b88f388c2640b2a86c265c71a57672
dor <dor.levy@compie.co.il>
2024-04-12 21:56:50 +0300

    .

--- config/discord.php
+++ config/discord.php
@@ -6,6 +6,6 @@ return [
     'client_secret' => env('DISCORD_CLIENT_SECRET'),
     'prefix'        => env('DISCORD_PREFIX'),
     'grand_type'    => env('DISCORD_GRAND_TYPE'),
-    'redirect_uri'  => "http://dont-know.test/api/auth",
+    'redirect_uri'  => env('DISCORD_REDIRECT_URI'),
     'scopes'        => env('DISCORD_SCOPES')
 ];
