c6c46082c16c5013e38c7628e98a51b09148b2a7
dor <dor.levy@compie.co.il>
2024-04-12 21:16:28 +0300

    .

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql_prod'),
 
     /*
     |--------------------------------------------------------------------------
@@ -39,14 +39,33 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'mysql_prod' => [
+            'driver'         => 'mysql',
+            'host'           => env('STACKHERO_MYSQL_HOST'),
+            'port'           => 3306,
+            'username'       => env('STACKHERO_MYSQL_USER'),
+            'password'       => env('STACKHERO_MYSQL_PASSWORD'),
+            'database'       => env('STACKHERO_MYSQL_USER'),
+            'charset'        => 'utf8mb4',
+            'collation'      => 'utf8mb4_unicode_ci',
+            'prefix'         => '',
+            'prefix_indexes' => true,
+            'strict'         => true,
+            'engine'         => null,
+            'sslmode'        => 'require',
+            'options'        => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mysql'     => [
             'driver'         => 'mysql',
             'url'            => env('DB_URL'),
-            'host'           => env('DB_HOST', '2wxwot.stackhero-network.com'),
+            'host'           => env('DB_HOST', '127.0.0.1'),
             'port'           => env('DB_PORT', '3306'),
-            'database'       => env('DB_DATABASE', 'fivem-panel'),
+            'database'       => env('DB_DATABASE', 'laravel'),
             'username'       => env('DB_USERNAME', 'root'),
-            'password'       => env('DB_PASSWORD', 'xhMScSS3EWY6hkrnM3eblLDo2v7Fkbif'),
+            'password'       => env('DB_PASSWORD', ''),
             'unix_socket'    => env('DB_SOCKET', ''),
             'charset'        => env('DB_CHARSET', 'utf8mb4'),
             'collation'      => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
