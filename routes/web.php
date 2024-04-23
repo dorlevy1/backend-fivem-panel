85750fd8c8b35d1ef50bda3e2364facbc5ad6c48
dor <dor.levy@compie.co.il>
2024-04-24 00:43:33 +0300

    .

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Events\DatabaseChange;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\DiscordController;
+use App\Http\Middleware\SubdomainEnsure;
 use App\Models\Player;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,18 +12,19 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Session;
 
+Route::domain('{subdomain}.' . env('APP_URL'))->middleware([SubdomainEnsure::class])->group(function () {
+    Route::get('test', function ($subdomain) {
+        dd($subdomain);
+        $this->inGameNotify = new DatabaseChange('inGame.215978', 'my-event');
 
-Route::get('test', function () {
-    $this->inGameNotify = new DatabaseChange('inGame.215978', 'my-event');
-
-    $this->inGameNotify->setData([
-        'type'    => 'KICK',
-        'message' => 'test',
-        'timeout' => 10000,
-    ]);
-    dd($this->inGameNotify->send($this->inGameNotify));
+        $this->inGameNotify->setData([
+            'type'    => 'KICK',
+            'message' => 'test',
+            'timeout' => 10000,
+        ]);
+        dd($this->inGameNotify->send($this->inGameNotify));
+    });
 });
-
 
 Route::get('/', function (Request $request) {
     return Player::all();
